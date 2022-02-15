7c3a28b71dc7eca3e091cc469ce2ab53a4694010
pratiksh404 <pratikdai404@gmail.com>
2022-02-15 21:39:29 +0545

    helper function string and number

---
+++ learning_php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Learning PHP</title>
+</head>
+
+<body>
+
+  <?php include 'backend/string.php'; ?>
+  <?php include 'backend/number.php'; ?>
+  <h1>TEST</h1>
+  <script>
+    console.log("Hello World");
+  </script>
+</body>
+
+</html>
