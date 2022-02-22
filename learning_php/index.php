3406beb3b461ced3ca85189426cdc9d6c04d9774
pratiksh404 <pratikdai404@gmail.com>
2022-02-22 21:41:39 +0545

    php form

--- learning_php/index.php
+++ learning_php/index.php
@@ -6,14 +6,35 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Learning PHP</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
 
-  <?php include 'backend/string.php'; ?>
-  <?php include 'backend/number.php'; ?>
-  <?php include 'backend/array.php'; ?>
-  <h1>TEST</h1>
+
+  <?php
+  $a = strtotime("tomorrow");
+  $date = date("Y-m-d", $a);
+  ?>
+
+  <div class="card m-5 p-5">
+    <h2 class="card-title">Form</h2>
+    <div class="card-body">
+      <form action="backend/form.php" method="POST">
+        <input type="text" name="name" id="" class="form-control" placeholder="Your name">
+        <br>
+        <input type="email" name="email" id="" class="form-control" placeholder="Your email">
+        <hr>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+
+  <h1 class="text-center">Today's date is <span class="text-primary"><?php echo $date; ?></span></h1>
+
+  <footer>
+    <h3 class="text-center">All right reserved @<span class="text-primary"><?php echo date('Y'); ?></span></h3>
+  </footer>
   <script>
     console.log("Hello World");
   </script>
