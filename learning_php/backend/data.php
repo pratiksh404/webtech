f611380ed462694b3af783f07f37c68a038ca7b3
pratiksh404 <pratikdai404@gmail.com>
2022-02-23 21:33:10 +0545

    dynamic website

---
+++ learning_php/backend/data.php
@@ -0,0 +1,8 @@
+<?php
+
+include 'metainfo.php';
+include 'feature.php';
+include 'service.php';
+include 'technology.php';
+include 'counter.php';
+include 'partner.php';
--- learning_php/backend/form.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-echo "<h1>Hello $name</h1>";
-echo "<br><h2>Your email is $email</h2>";
